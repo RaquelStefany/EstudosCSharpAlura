f6866e89f45932eb3e724f2c6628593fa9cd9e58
Raquel Rodrigues <raquelgeneroso@fenti.com.br>
2025-05-28 23:04:23 -0300

    Iniciando Modulo 02

---
+++ 02 - C# aplicando a Orientação a Objetos/ScrrenSound/Musica.cs
@@ -0,0 +1,16 @@
+﻿class Musica 
+{ 
+    public string Nome { get; set; }
+    public string Artista { get; set; }
+    public int Duracao { get; set; }
+    public bool Disponivel { get; set; }
+
+
+    public void ExibirDetalhes() 
+    {
+        Console.WriteLine($"Música: {Nome}");
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Duração: {Duracao} segundos");
+        Console.WriteLine($"Disponível: {(Disponivel ? "Sim" : "Não")}");
+    }
+}
---
+++ 02 - C# aplicando a Orientação a Objetos/ScrrenSound/Program.cs
@@ -0,0 +1,16 @@
+﻿Musica musica1 = new Musica();
+musica1.nome = "Bohemian Rhapsody";
+musica1.artista = "Queen";
+musica1.duracao = 354; // duração em segundos
+musica1.DefinirDisponibilidade(true); // Definindo a música como disponível
+
+
+Musica musica2 = new Musica();
+musica2.nome = "Heathens";
+musica2.artista = "Twenty One Pilots";
+musica2.duracao = 200; // duração em segundos
+musica2.DefinirDisponibilidade(true); // Definindo a música como disponível
+
+musica1.ExibirDetalhes();
+Console.WriteLine(); // Linha em branco para separar as exibições
+musica2.ExibirDetalhes();
